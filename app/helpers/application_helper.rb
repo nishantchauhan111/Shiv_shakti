af511b4fd05830e0735598713862694bb97e6a96
NC-Proffecional <nc-proffecional@Nishants-MacBook-Pro-2.local>
2026-04-01 12:43:11 +0530

    MADE COMMON HELPER METHOD FOR THE FOOTER LINES AND IMAGES

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  def footer_lines
+    [
+      "We KNOW EVERYTHING YOU NEED aBOUT PROPERTY",
+      "Mr. Kuldeepsinh Gohil",
+      "Founder Shivshakti group"
+    ]
+  end
+
+  def footer_img
+    [
+      asset_path("tge.png"),
+      asset_path("portrait_1.png")
+    ]
+  end
+
+  def contact
+    [
+      "shivshaktigroup@gmail.com",
+      "+91 9023187500",
+      "https://share.google/7sbwZQlDz2kyGZmdW",
+      "Everyday  10 - 18",
+      "https://www.linkedin.com/company/sidthailand/",
+      "https://www.facebook.com/sidphuket",
+      "https://www.instagram.com/shivshaktii_group/"
+    ]
+  end
 end
